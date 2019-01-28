fb71ad2811f63c3351dc04be42ab119c33fef359
ydw0113 <ydw0113@users.noreply.github.com>
2019-01-28 15:03:03 +0900

    1

---
+++ graphql/db.js
@@ -0,0 +1,50 @@
+export let movies = [
+  {
+    id: 0,
+    name: "Star Wars - The new one",
+    score: 1
+
+  },
+    {
+      id: 1,
+      name: "Avengers - The new one",
+      score: 8
+
+    },
+    {
+      id: 2,
+      name: "The Godfather 1",
+      score: 99
+
+    },
+    {
+      id: 3,
+      name: "Logan",
+      score: 2
+
+    },
+];
+export const getmovies = () => movies;
+
+export const getById = id => {
+  const filteredMovies = movies.filter(movie => movie.id === id);
+  return filteredMovies[0];
+}
+export const deleteMovie = (id) => {
+  const cleanedMovies = movies.filter(movie => movie.id !== id);
+  if(movies.length > cleanedMovies.length){
+    movies = cleanedMovies;
+    return true;
+  }else {
+    return false;
+  }
+}
+export const addMovie = (name, score) => {
+    const newMovie = {
+      id: `${movies.length+1 }`,
+      name,
+      score
+    };
+    movies.push(newMovie);
+    return newMovie;
+};
---
+++ graphql/resolvers.js
@@ -0,0 +1,15 @@
+import {getmovies, getById, addMovie, deleteMovie} from "./db";
+
+
+const resolvers = {
+    Query: {
+      movies:() => getmovies(),
+      movie: (_, { id }) => getById(id)
+    },
+    Mutation: {
+      addMovie: (_, {name,score}) => addMovie(name, score),
+      deleteMovie: (_, {id}) => deleteMovie(id)
+    }
+};
+
+export default resolvers;
